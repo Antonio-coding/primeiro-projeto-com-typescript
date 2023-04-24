9bd1bae8ebdc6db6155bba4a00ba8249b0212176
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-04-24 13:11:09 -0300

    continuação dos dos estudos

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,29 +1,23 @@
+"use strict";
 //string,boolean, number, ...
-let x:number = 10;
-
+let x = 10;
 x = 20;
-
 console.log(x);
-
 //inferencia x annotation
 let y = 12;
 // y = "teste"
-
-let z: number=12;
-
+let z = 12;
 //tipos bassicos 
-let firstName: string =  "antonio";
-let age : number = 25;
-const isAdmin: boolean = true;
-
+let firstName = "antonio";
+let age = 25;
+const isAdmin = true;
 // string != string
-
-console.log (typeof firstName);
+console.log(typeof firstName);
 // object
-const myNumbers: number [] = [1,2,3]
-console.log (myNumbers);
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
 console.log(myNumbers.length);
 //console.log(myNumbers.toUpperCase()) é um erro que o programa aponta
 console.log(firstName.toUpperCase());
 myNumbers.push(5);
-console.log (myNumbers);
+console.log(myNumbers);
